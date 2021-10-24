1c79a2de65ae4eae7b50d31ee84939095e02f781
Jamie Dorst <Jamie@Jamie.local>
2021-10-24 13:56:43 -0400

    attempt 6 to fix homework_6a folder

---
+++ homework_6a/README.md
@@ -0,0 +1,7 @@
+# View the Website
+
+https://jamiedorst-pui-fluffstuff.netlify.app/
+
+## Extra Credit
+
+All product pages are accessible and I'm not sure what counts as "interesting and useful" Javascript (per the extra credit section on the assignment) but I did a lot of work to actually utilize React components to reduce code repitition and make more functionality (which was all very new and useful to me).
---
+++ homework_6a/src/App.js
@@ -0,0 +1,45 @@
+import "./App.css";
+import Homepage from "./homepage.js";
+import Browse from "./browse.js";
+import Product from "./couch.js";
+import About from "./about.js";
+import Faq from "./faq.js";
+import Contact from "./contact.js";
+import Cart from "./cart.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div id="App">
+      {/* router to link my pages together */}
+      <Router>
+        <Switch>
+          <Route exact path="/Products" component={Browse} />
+          <Route
+            exact
+            path="/CouchPillow"
+            component={() => <Product id={0} />}
+          />
+          <Route exact path="/BedPillow" component={() => <Product id={1} />} />
+          <Route
+            exact
+            path="/RoundPillow"
+            component={() => <Product id={2} />}
+          />
+          <Route
+            exact
+            path="/FloorPoufPillow"
+            component={() => <Product id={3} />}
+          />
+          <Route exact path="/Cart" component={Cart} />
+          <Route exact path="/About" component={About} />
+          <Route exact path="/FAQs" component={Faq} />
+          <Route exact path="/Contact" component={Contact} />
+          <Route exact path="/" component={Homepage} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
